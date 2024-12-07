e2a55012d8c38fe53edf80684e813ac727a9151c
Avinash <anjalirajesh1412089@gmail.com>
2024-12-07 20:43:52 +0530

    added intitial files

---
+++ script.js
@@ -0,0 +1,19 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add() {
+    if (inputs.value == "") {
+        alert("Please Enter Task")
+    } else {
+        let newEle = document.createElement("ul");
+        newEle.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        inputs.value = "";
+        newEle.querySelector("i").addEventListener("click", remove);
+        console.log(newWle.value)
+
+        function remove() {
+            newEle.remove()
+        }
+    }
+}
